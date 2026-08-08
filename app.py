082a4877e914943a584774a1c0986aa2f28a9dcc
khushabupatrikar7-code <khushabupatrikar7@gmail.com>
2026-08-08 13:44:54 +0530

    Added nav bar in base.html and add task in  index.html

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from flask import Flask  
+from flask import Flask  ,render_template
+
+app = Flask(__name__)
+
+todos =[
+    {"sno": 1, "title": "Sample task", "desc": "This is a sample task  for to do list", "status": "Pending"}
+
+]
+
+@app.route("/") #decorator
+def home():
+    return render_template("index.html")
 
 
 
+if(__name__ == "__main__"):
+    app.run(debug=True)
