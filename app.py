57f1e00051e0da205f661863fd660676b2b93a18
khushabupatrikar7-code <khushabupatrikar7@gmail.com>
2026-08-08 15:18:53 +0530

    added update.html

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask  ,render_template
 app = Flask(__name__)
 
 todos =[
-    {"sno": 1, "title": "Sample task", "desc": "This is a sample task  for to do list", "status": "Pending"}
+    {"sr no": 1, "title": "Sample task", "desc": "This is a sample task  for to do list", "date_created":"08-08-2026","status": "Pending"}
 
 ]
 
 @app.route("/") #decorator
 def home():
-    return render_template("index.html")
+    return render_template("index.html", allTodos = todos)
 
 
 
 if(__name__ == "__main__"):
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
